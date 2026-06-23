ac9ecc88926161cacafa51983cd0335db409eb40
Efren Gutierrez Torres <efrengutierreztorres@gmail.com>
2026-06-22 21:21:50 -0600

    creacion cuenta de resend con correo maxdent

--- routes/citas.js
+++ routes/citas.js
@@ -21,33 +21,42 @@ server.get("/:fecha/:sucursal", async (req, res) => {
 
 server.post("/", async (req, res) => {
   try {
+    console.log("KEY:", process.env.RESEND_API_KEY); // 👈 AQUÍ
+
     const { fecha, horario, sucursal } = req.body;
+
     const existe = await Cita.findOne({
       fecha,
       horario,
       sucursal,
     });
+
     if (existe) {
       return res.status(400).json({
         mensaje: "Ese horario ya está ocupado en esa sucursal",
       });
     }
+
     const cita = new Cita(req.body);
     await cita.save();
 
-    await resend.emails.send({
+    const { data, error } = await resend.emails.send({
       from: "onboarding@resend.dev",
       to: "maxdent.citas@gmail.com",
       subject: "Nueva cita agendada",
       html: `
-    <h2>Nueva cita</h2>
-    <p><b>Nombre:</b> ${req.body.nombre}</p>
-    <p><b>Teléfono:</b> ${req.body.telefono}</p>
-    <p><b>Sucursal:</b> ${req.body.sucursal}</p>
-    <p><b>Fecha:</b> ${req.body.fecha}</p>
-    <p><b>Horario:</b> ${req.body.horario}</p>
-  `,
+        <h2>Nueva cita</h2>
+        <p><b>Nombre:</b> ${req.body.nombre}</p>
+        <p><b>Teléfono:</b> ${req.body.telefono}</p>
+        <p><b>Sucursal:</b> ${req.body.sucursal}</p>
+        <p><b>Fecha:</b> ${req.body.fecha}</p>
+        <p><b>Horario:</b> ${req.body.horario}</p>
+      `,
     });
+
+    console.log("RESEND ERROR:", error);
+    console.log("RESEND DATA:", data);
+
     res.status(201).json({
       mensaje: "Cita creada correctamente",
     });
